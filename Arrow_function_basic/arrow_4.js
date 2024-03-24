30712d32a9fbed15c3e38c07a3b73116665e3d0d
Deepak Khanka -X (dkhanka) <dkhanka@cisco.com>
2024-03-24 23:03:34 +0530

    arrow function

---
+++ Arrow_function_basic/arrow_4.js
@@ -0,0 +1,16 @@
+// Arrow function demo programs.
+
+function check(a, b) {
+  return num1 + num2;
+}
+
+let sum2 = (num1, num2) => {
+  return num1 + num2;
+};
+
+let mul = (num1, num2) => num1 * num2;
+
+// calling function.
+
+console.log(mul(3, 4));
+console.log(sum2(1000, 2000));
