f5a99e78c5cba51ecf4e5c1dbc2ebf75b59ec757
Deepak Khanka -X (dkhanka) <deepakkhanka.97@gmail.com>
2024-04-08 15:17:41 +0530

    FizzBuzz problem in JS

---
+++ DSA_Assign/fizz_buzz.js
@@ -0,0 +1,15 @@
+// var num = 10;
+// num = "Hello";
+// console.log(num);
+
+// const willRain = true;
+// if (!willRain || 3 === 3) {
+//   console.log("Wear Raincoat");
+// } else {
+//   console.log("Do not wear a raincoat");
+// }
+
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+  continue;
+}
