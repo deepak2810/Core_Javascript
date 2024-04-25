c03ae9957a49b5d3c39db43839b93805910c84f7
Deepak Khanka -X (dkhanka) <deepakkhanka.97@gmail.com>
2024-04-25 19:27:11 +0530

    map function in JS

--- MAP/map01.js
+++ MAP/map01.js
@@ -1,9 +1,14 @@
 let arr = [1, 2, 3, 4, 5];
 
 const squares = arr.map(square);
+const cubes = arr.map(cubes);
 
-console.log(squares);
+console.log(cubes);
 
 function square(n) {
   return Math.pow(n, 2);
+}
+
+function cubes(n) {
+  return Math.pow(n, 3);
 }
