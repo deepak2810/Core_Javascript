e852a691ab4adcf626505da1318e94a4151d8e86
Deepak Khanka -X (dkhanka) <deepakkhanka.97@gmail.com>
2024-04-26 16:04:28 +0530

    map baisc program

---
+++ MAP/map00.js
@@ -0,0 +1,12 @@
+let employee = new Map();
+
+employee.set("Deepak ", "A1");
+employee.set("Chris", "B1");
+employee.set("Rex", "B2");
+
+// To perform the delete operation.
+
+employee.delete("Rex");
+
+//to print the map.
+console.log(employee);
