d1710003984750329c63fd5fb58ed84b91e8ff80
Deepak Khanka -X (dkhanka) <deepakkhanka.97@gmail.com>
2024-04-26 16:11:35 +0530

    ..

--- MAP/map00.js
+++ MAP/map00.js
@@ -1,6 +1,6 @@
 let employee = new Map();
 
-employee.set("Deepak ", "A1");
+employee.set("Deepak", "A1");
 employee.set("Chris", "B1");
 employee.set("Rex", "B2");
 
@@ -10,3 +10,7 @@ employee.delete("Rex");
 
 //to print the map.
 console.log(employee);
+
+// To check if a value is present or not : check it by has method.
+
+console.log(employee.has("Deepak"));
